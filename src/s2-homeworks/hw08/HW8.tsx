e8791cf6c40305fb87a644b514d005c2c55b4890
paul-koo <pavelkoltun999@gmail.com>
2024-10-19 22:49:43 +0300

    feat: completed hw8 (not styles)

--- src/s2-homeworks/hw08/HW8.tsx
+++ src/s2-homeworks/hw08/HW8.tsx
@@ -1,57 +1,57 @@
-import React, {useState} from 'react'
-import {homeWorkReducer} from './bll/homeWorkReducer'
-import s from './HW8.module.css'
-import s2 from '../../s1-main/App.module.css'
-import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
-import User from './User'
+import React, { useState } from 'react';
+import { homeWorkReducer } from './bll/homeWorkReducer';
+import s from './HW8.module.css';
+import s2 from '../../s1-main/App.module.css';
+import SuperButton from '../hw04/common/c2-SuperButton/SuperButton';
+import User from './User';
 
 /*
-* 1 - дописать типы и логику (сортировка по имени, фильтрация по совершеннолетию) homeWorkReducer, проверить тестом
-* 2 - дописать компоненту User
-* 3 - сделать стили в соответствии с дизайном
-* */
+ * 1 - дописать типы и логику (сортировка по имени, фильтрация по совершеннолетию) homeWorkReducer, проверить тестом
+ * 2 - дописать компоненту User
+ * 3 - сделать стили в соответствии с дизайном
+ * */
 
 export type UserType = {
-    _id: number
-    name: string
-    age: number
-}
+    _id: number;
+    name: string;
+    age: number;
+};
 
-const initialPeople: UserType[] = [
+export const initialPeople: UserType[] = [
     // студенты могут поменять имя/возраст/количество объектов, _id должны быть целочисленные
-    {_id: 0, name: 'Кот', age: 3},
-    {_id: 1, name: 'Александр', age: 66},
-    {_id: 2, name: 'Коля', age: 16},
-    {_id: 3, name: 'Виктор', age: 44},
-    {_id: 4, name: 'Дмитрий', age: 40},
-    {_id: 5, name: 'Ирина', age: 55},
-]
+    { _id: 0, name: 'Кот', age: 3 },
+    { _id: 1, name: 'Александр', age: 66 },
+    { _id: 2, name: 'Коля', age: 16 },
+    { _id: 3, name: 'Виктор', age: 44 },
+    { _id: 4, name: 'Дмитрий', age: 40 },
+    { _id: 5, name: 'Ирина', age: 55 },
+];
 
 const HW8 = () => {
-    const [people, setPeople] = useState<UserType[]>(initialPeople)
-    const [currentSort, setCurrentSort] = useState('')
+    const [people, setPeople] = useState<UserType[]>(initialPeople);
+    const [currentSort, setCurrentSort] = useState('');
 
-    const finalPeople = people.map((u: UserType) => <User key={u._id} u={u}/>)
+    const finalPeople = people.map((u: UserType) => <User key={u._id} u={u} />);
 
     const sortUp = () => {
         setPeople(
-            homeWorkReducer(initialPeople, {type: 'sort', payload: 'up'})
-        ) // в алфавитном порядке a.name > b.name
-        setCurrentSort('up')
-    }
+            homeWorkReducer(initialPeople, { type: 'sort', payload: 'up' })
+        ); // в алфавитном порядке a.name > b.name
+        setCurrentSort('up');
+    };
 
     const sortDown = () => {
         setPeople(
-            homeWorkReducer(initialPeople, {type: 'sort', payload: 'down'})
-        ) // в обратном порядке a.name < b.name}
-        setCurrentSort('down')
-    }
+            homeWorkReducer(initialPeople, { type: 'sort', payload: 'down' })
+        ); // в обратном порядке a.name < b.name}
+        setCurrentSort('down');
+    };
     const check18 = () => {
         setPeople(
-            homeWorkReducer(initialPeople, {type: 'check', payload: 18})
-        ) // совершеннолетние
-        setCurrentSort('18')
-    }
+            homeWorkReducer(initialPeople, { type: 'check', payload: 18 })
+        ); // совершеннолетние
+        setCurrentSort('18');
+    };
 
     return (
         <div id={'hw3'}>
@@ -84,10 +84,10 @@ const HW8 = () => {
 
                     <table id={'hw8-users'} className={s.users}>
                         <thead className={s.thead}>
-                        <tr>
-                            <td className={s.nameCol}>Name</td>
-                            <td className={s.ageCol}>Age</td>
-                        </tr>
+                            <tr>
+                                <td className={s.nameCol}>Name</td>
+                                <td className={s.ageCol}>Age</td>
+                            </tr>
                         </thead>
 
                         <tbody>{finalPeople}</tbody>
@@ -95,7 +95,7 @@ const HW8 = () => {
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default HW8
+export default HW8;
