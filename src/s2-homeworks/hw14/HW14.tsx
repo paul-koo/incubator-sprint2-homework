b489ae2c26f7a4dcbba0e69f498ecff15bd06b0b
paul-koo <pavelkoltun999@gmail.com>
2025-05-10 00:15:31 +0300

    feat: completed hw14

--- src/s2-homeworks/hw14/HW14.tsx
+++ src/s2-homeworks/hw14/HW14.tsx
@@ -1,68 +1,73 @@
-import React, {useEffect, useState} from 'react'
-import s2 from '../../s1-main/App.module.css'
-import s from './HW14.module.css'
-import axios from 'axios'
-import SuperDebouncedInput from './common/c8-SuperDebouncedInput/SuperDebouncedInput'
-import {useSearchParams} from 'react-router-dom'
+import React, { useEffect, useState } from 'react';
+import s2 from '../../s1-main/App.module.css';
+import s from './HW14.module.css';
+import axios from 'axios';
+import SuperDebouncedInput from './common/c8-SuperDebouncedInput/SuperDebouncedInput';
+import { useSearchParams } from 'react-router-dom';
 
 /*
-* 1 - дописать функцию onChangeTextCallback в SuperDebouncedInput
-* 2 - дописать функцию sendQuery в HW14
-* 3 - дописать функцию onChangeText в HW14
-* 4 - сделать стили в соответствии с дизайном
-* 5 - добавить HW14 в HW5/pages/JuniorPlus
-* */
-
+ * 1 - дописать функцию onChangeTextCallback в SuperDebouncedInput
+ * 2 - дописать функцию sendQuery в HW14
+ * 3 - дописать функцию onChangeText в HW14
+ * 4 - сделать стили в соответствии с дизайном
+ * 5 - добавить HW14 в HW5/pages/JuniorPlus
+ * */
+//{ techs: string[] }
 const getTechs = (find: string) => {
     return axios
-        .get<{ techs: string[] }>(
-            'https://samurai.it-incubator.io/api/3.0/homework/test2',
-            {params: {find}}
-        )
-        .catch((e) => {
-            alert(e.response?.data?.errorText || e.message)
+        .get<{ techs: string[] }>('https://samurai.it-incubator.io/api/3.0/homework/test2', {
+            params: { find },
         })
-}
+        .catch((e) => {
+            alert(e.response?.data?.errorText || e.message);
+        });
+};
 
 const HW14 = () => {
-    const [find, setFind] = useState('')
-    const [isLoading, setLoading] = useState(false)
-    const [searchParams, setSearchParams] = useSearchParams()
-    const [techs, setTechs] = useState<string[]>([])
+    const [find, setFind] = useState('');
+    const [isLoading, setLoading] = useState(false);
+    const [searchParams, setSearchParams] = useSearchParams();
+    const [techs, setTechs] = useState<string[]>([]);
 
     const sendQuery = (value: string) => {
-        setLoading(true)
-        getTechs(value)
-            .then((res) => {
-                // делает студент
+        setLoading(true);
+        getTechs(value).then((res) => {
+            if (res) {
+                const filter = res.data.techs.filter((el) => el.includes(find));
+                console.log(filter);
+                setTechs(filter);
+            }
 
-                // сохранить пришедшие данные
+            setLoading(false);
 
-                //
-            })
-    }
+            // делает студент
+            // сохранить пришедшие данные
+            //
+        });
+    };
 
     const onChangeText = (value: string) => {
-        setFind(value)
+        setFind(value);
+        setSearchParams(value);
         // делает студент
 
         // добавить/заменить значение в квери урла
         // setSearchParams(
 
         //
-    }
+    };
 
     useEffect(() => {
-        const params = Object.fromEntries(searchParams)
-        sendQuery(params.find || '')
-        setFind(params.find || '')
-    }, [])
+        const params = Object.fromEntries(searchParams);
+        sendQuery(params.find || '');
+        setFind(params.find || '');
+    }, []);
 
-    const mappedTechs = techs.map(t => (
+    const mappedTechs = techs.map((t) => (
         <div key={t} id={'hw14-tech-' + t} className={s.tech}>
             {t}
         </div>
-    ))
+    ));
 
     return (
         <div id={'hw14'}>
@@ -77,13 +82,13 @@ const HW14 = () => {
                 />
 
                 <div id={'hw14-loading'} className={s.loading}>
-                    {isLoading ? '...ищем' : <br/>}
+                    {isLoading ? '...ищем' : <br />}
                 </div>
 
                 {mappedTechs}
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default HW14
+export default HW14;
